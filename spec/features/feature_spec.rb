13db65aafe13cd941defabe7293d59bfa49c82d4
Hanif Fakira <fakira.work@gmail.com>
2016-08-02 11:51:11 +0100

    2nd feature test written

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -15,3 +15,21 @@ feature 'User story 1' do
     expect(actual_price).to eq 74.2
   end
 end
+
+feature 'User story 2' do
+  let(:checkout) {Checkout.new}
+  let(:item001) { Item.new( {product_code: '001', name: 'Lavender heart', price: 9.25} ) }
+  let(:item002) { Item.new( {product_code: '002', name: 'Personalised cufflinks', price: 45.0} ) }
+  let(:item003) { Item.new( {product_code: '003', name: 'Kids T-shirt', price: 19.95 } ) }
+
+  scenario 'give discount when total greater than 60' do
+    checkout.scan(item001)
+    checkout.scan(item002)
+    checkout.scan(item003)
+
+    actual_price = checkout.total
+
+    expect(actual_price).to eq 66.78
+  end
+
+end
