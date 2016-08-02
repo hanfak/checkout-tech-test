bbeb3c80f00090ff35f6ee48f52b0b85812c926a
Hanif Fakira <fakira.work@gmail.com>
2016-08-02 13:42:12 +0100

    US2 refactored promo rules attributes
    - used hash for initialize parameters
    - extra tests for PromotionRules, check discount returned if positive

--- lib/promotion_rules.rb
+++ lib/promotion_rules.rb
@@ -1,13 +1,13 @@
 class PromotionRules
   attr_reader :total_discount_limit
 
-  def initialize( total_discount_limit = 0.0, total_discount = 0.0)
-    @total_discount_limit = total_discount_limit
-    @total_discount = total_discount
+  def initialize( options = {})
+    @total_discount_limit = options[:total_discount_limit] || 0.0
+    @total_discount = options[:total_discount]
   end
 
   def total_discount
-    return 0.0 if total_discount_limit == 0.0
+    return 0.0 if total_discount_limit <= 0.0
     @total_discount
   end
 end
--- spec/promotion_rules_spec.rb
+++ spec/promotion_rules_spec.rb
@@ -1,6 +1,7 @@
 describe PromotionRules do
   subject(:promotion_rules) {described_class.new}
-  subject(:promotion_rules_discount) {described_class.new(60.0, 0.1)}
+  subject(:promotion_rules_discount) {described_class.new( {total_discount_limit: 60.0, total_discount: 0.1} )}
+  subject(:promotion_rules_no_limit) {described_class.new( {total_discount: 0.1} )}
 
   describe 'defaults' do
     it 'has no discount' do
@@ -13,8 +14,18 @@ describe PromotionRules do
   end
 
   describe '#total_discount' do
+    subject(:promotion_rules_negative_limit) {described_class.new( {total_discount: 0.1, total_discount_limit: - 60.0 } )}
+
     it 'instantiated with discount amount' do
       expect(promotion_rules_discount.total_discount).to eq 0.1
+    end
+
+    it 'need discount limit to have discount' do
+      expect(promotion_rules_no_limit.total_discount).to eq 0.0
+    end
+
+    it 'need positive discount limit to have discount' do
+      expect(promotion_rules_negative_limit.total_discount).to eq 0.0
     end
   end
 
