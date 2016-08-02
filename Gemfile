636596d8f398a477c226806c9698a02dd57f5258
Hanif Fakira <fakira.work@gmail.com>
2016-08-02 09:44:32 +0100

    update gemfile

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source "http://rubygems.org"
+
+ruby '2.2.3'
+
+gem 'coveralls', require: false
+
+group :test do
+  gem 'rspec'
+  gem 'rspec-mocks'
+  gem 'capybara'
+end
---
+++ spec/features/feature_spec.rb
@@ -0,0 +1,16 @@
+feature 'User story 1' do
+  scenario 'find total of basket' do
+    let(:checkout) {Checkout.new}
+    let(:item1) {Item.new(1, 'Lavender heart ', 9.25 )}
+    let(:item2) {Item.new(2, 'Personalised cufflinks', 45.0)}
+    let(:item3) {Item.new(3, 'Kids T-shirt', 19.95 )}
+
+    checkout.scan(item1)
+    checkout.scan(item2)
+    checkout.scan(item3)
+
+    actual_price = checkout.total
+
+    expect(actual_price).to eq 74.2
+  end
+end
